eb0057b051eff05620618d197e4574406911123f
Stilan <40999153+Stilan@users.noreply.github.com>
2022-03-22 19:01:31 +0700

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/Store.java
+++ src/main/java/ru/job4j/di/Store.java
@@ -2,10 +2,11 @@ package ru.job4j.di;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class Store {
     private List<String> data = new ArrayList<String>();
 
