e02cf269711a1f8cab98bac9580a10988a557931
Stilan <40999153+Stilan@users.noreply.github.com>
2022-03-22 18:24:11 +0700

    2. Сканирование проекта [#1070]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -5,13 +5,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(Store.class);
-        context.register(ConsoleInput.class);
-        context.register(StartUI.class);
+        context.scan("ru.job4j.di");
         context.refresh();
         StartUI ui = context.getBean(StartUI.class);
-        ConsoleInput ci = context.getBean(ConsoleInput.class);
-        ui.add(ci.askStr("Hello"));
+        ui.consoleInput("Hello ");
         ui.add("Petr Arsentev");
         ui.add("Ivan ivanov");
         ui.print();
--- src/main/java/ru/job4j/di/Store.java
+++ src/main/java/ru/job4j/di/Store.java
@@ -2,7 +2,10 @@ package ru.job4j.di;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Store {
     private List<String> data = new ArrayList<String>();
 
